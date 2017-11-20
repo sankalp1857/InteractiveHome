1e5901f6c6bfbf83bf29dcf91227cf42e17baece
Sankalp Mehra <sankalpmehra.mehra@gmail.com>
2017-11-21 01:46:03 +0530

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -3,10 +3,8 @@ import datetime
 
 now = datetime.datetime.now()
 
-
 def alarm(time):
-    os.system("alarmclock.sh -t " + time + "&")
-
+    os.system("nohup ./unix-alarm-clock/alarmclock.sh -t " + time + " & printf \'%s " + time + "\n\' \"$!\" >>/home/pi/Documents/InteractiveHome/alarm_pid.txt")
 
 def reminder(day, month, year, msg):
     cur_day = now.day
@@ -15,8 +13,7 @@ def reminder(day, month, year, msg):
     cur_date = datetime.date(cur_year, cur_month, cur_day)
     rem_date = datetime.date(year, month, day)
     delta = rem_date - cur_date
-    os.system("alarmclock.sh -d "+str(delta.days)+" -m " + msg + " &")
-    os.system("printf \'%s " + msg + "\n\' \"$!\" >> /data/reminder_pid.txt")
+    os.system("nohup ./unix-alarm-clock/alarmclock.sh -d "+str(delta.days)+" -m " + msg + " & "+"printf \'%s " + msg + "\n\' \"$!\" >> /data/reminder_pid.txt")
 
 
 def todo(message):
@@ -32,4 +29,17 @@ def cancel_reminder(msg):
     for s in lines:
         if msg not in s:
             f.write(s)
+    f.close()
+
+def cancel_alarm(time):
+    with open('alarm_pid.txt') as f:
+        lines = f.readlines()
+    f.close()
+    f = open('alarm_pid.txt', 'w')
+    for s in lines:
+        if time not in s:
+            f.write(s)
+        else:
+            pid =s.partition(' ')[0]
+            os.system("kill " + pid)
     f.close()
